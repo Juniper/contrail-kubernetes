13b71dcc8a4b5295c346ed5c0a27671d0afcbb68
Ananth Suryanarayana <anantha@juniper.net>
2015-07-09 12:11:24 -0700

    Update kubectl web www dir path

--- scripts/opencontrail-install/contrail_install.rb
+++ scripts/opencontrail-install/contrail_install.rb
@@ -434,7 +434,7 @@ def provision_contrail_controller_kubernetes
     # http://localhost:8001/static/app/#/dashboard/
     sh("ln -sf /usr/local/bin/kubectl /usr/bin/kubectl", true)
     if File.file? "/usr/local/bin/kubectl"
-        sh("nohup /usr/local/bin/kubectl proxy --www=#{@ws}/build_kubernetes/www 2>&1 > /var/log/kubectl-web-proxy.log", true, 1, 1, true)
+        sh("nohup /usr/local/bin/kubectl proxy --www=#{ENV["HOME"]}/contrail/kubernetes/www 2>&1 > /var/log/kubectl-web-proxy.log", true, 1, 1, true)
     end
 
     target = @platform =~ /fedora/ ? "/root" : "/home/#{@opt.user}"
