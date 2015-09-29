cd5cf3817655fcfadb4869aec40e5fb07de93d61
Sanju Abraham <asanju@gmail.com>
2015-09-28 20:04:50 -0700

    check_docker only for non kubernetes setups

--- cluster/provision_minion.sh
+++ cluster/provision_minion.sh
@@ -435,8 +435,7 @@ function check_docker()
    service docker restart
    docpid=`pidof docker`
   fi
-
-  if  [ -z "$docpid" ]; then
+  if  [ -z "$docpid" ] && [ ! -f "/usr/local/bin/kubelet" ]; then
     (/usr/bin/docker -d -p /var/run/docker.pid --bridge=cbr0 --iptables=false --ip-masq=false)&
   fi
 }
