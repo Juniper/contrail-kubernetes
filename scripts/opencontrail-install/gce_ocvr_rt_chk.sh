595bfbe312065e0f64ecf9d9b7c5981e7ca883ba
Sanju Abraham <asanju@gmail.com>
2015-09-30 01:05:47 -0700

    updated with additional checks for omitted flags in rt dump

--- scripts/opencontrail-install/gce_ocvr_rt_chk.sh
+++ scripts/opencontrail-install/gce_ocvr_rt_chk.sh
@@ -15,16 +15,27 @@ naddr=$(/usr/local/bin/gcloud compute networks list | grep default | awk '{print
 prefix=$(echo $naddr | cut -d/ -f1)
 len=$(echo $naddr | cut -d/ -f2)
 
-rtdata32=$(/usr/bin/rt --dump 0 | grep $OPENCONTRAIL_CONTROLLER_IP | awk '{print $5}' | head -1)
+rtdata32=$(/usr/bin/rt --dump 0 | grep $OPENCONTRAIL_CONTROLLER_IP/32 | awk '{print $5}' | head -1)
+if [ "$rtdata32" == "-" ]; then
+   rtdata32=$(rt --dump 0 |grep $OPENCONTRAIL_CONTROLLER_IP/32 | awk '{print $4}' | head -1)
+fi
 oc=$OPENCONTRAIL_CONTROLLER_IP
 sub=$(echo ${oc%.*} ${oc##*.}  | awk '{print $1}').0
 rtdata24=$(/usr/bin/rt --dump 0 | grep $sub | awk '{print $5}' | head -1)
+if [ "$rtdata24" == "-" ]; then
+    rtdata24=$(rt --dump 0 |grep $sub | awk '{print $4}' | head -1)
+fi
 if [ ! -z $rtdata32 ] && [ ! -z $rtdata24 ]; then
   if [ "$rtdata32" != 1000 ] || [ "$rtdata24" != 1000 ]; then
      nhid=$(/usr/bin/rt --dump 0 | grep $OPENCONTRAIL_CONTROLLER_IP | awk '{print $5}' | head -1)
+     if [ "$nhid" == "-" ]; then
+        nhid=$(/usr/bin/rt --dump 0 | grep $OPENCONTRAIL_CONTROLLER_IP | awk '{print $4}' | head -1)
+     fi
      /usr/bin/nh --delete $nhid
      /usr/bin/nh --create 1000 --type 2 --smac $vmac --dmac $gwmac --oif $intf
+     sleep 2
      /usr/bin/rt -d -f AF_INET -r $len -p $OPENCONTRAIL_CONTROLLER_IP -l 32 -n $nhid -v 0
+     sleep 2
      /usr/bin/rt -c -f AF_INET -n 1000 -p $prefix -l $len -v 0
   fi
 fi
