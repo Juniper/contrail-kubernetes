661f8b4bd8861f06194f503f12047984ae6e9adf
Sanju Abraham <asanju@gmail.com>
2015-10-06 15:01:18 -0700

    adjusting counter to check ifmap state in contrail control

--- cluster/provision_master.sh
+++ cluster/provision_master.sh
@@ -94,11 +94,15 @@ function check_contrail_services()
    fi
 
   cc=''
-  for (( i=0; i<20; i++ ))
+  ifmapup=false
+  for (( i=0; i<5; i++ ))
     do
-     cc=$(curl -s http://localhost:8083/Snh_SandeshUVECacheReq?tname=NodeStatus | xmllint --format - | grep -ow "contrail-control")
-     ifmapup=$(curl -s http://localhost:8083/Snh_IFMapPeerServerInfoReq? | xmllint --format - | grep end_of_rib_computed | cut -d ">" -f2 | cut -d "<" -f1)
-     if [ ! -z $cc ] && $ifmapup ; then
+     cc=$(curl -s http://$OPENCONTRAIL_CONTROLLER_IP:8083/Snh_SandeshUVECacheReq?tname=NodeStatus | xmllint --format - | grep -ow "contrail-control")
+     if [ ! -z $cc ]; then
+         ifmapup=$(curl -s http://$OPENCONTRAIL_CONTROLLER_IP:8083/Snh_IFMapPeerServerInfoReq? | xmllint --format - | grep end_of_rib_computed | cut -d ">" -f2 | cut -d "<" -f1)
+     fi
+     if [ "$ifmapup" == true ]; then
+       log_info_msg "IFMAP and Contrail-Control are up are ready for processing agent subscriptions"
        break
      fi
      sleep 3
