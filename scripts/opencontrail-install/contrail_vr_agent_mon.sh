137319b82d7daa68e753b98901c7f010a71221dd
Sanju Abraham <asanju@gmail.com>
2015-10-09 02:05:09 -0700

    logs messages added/modified

--- scripts/opencontrail-install/contrail_vr_agent_mon.sh
+++ scripts/opencontrail-install/contrail_vr_agent_mon.sh
@@ -9,11 +9,9 @@ img=$(docker images | grep vrouter | awk '{print $1}')
 ver=$(docker images | grep vrouter | awk '{print $2}')
 if [[ -n "$docid" ]] && [ "$xmpp" != "ESTABLISHED" ]; then
    docker restart $docid
+   echo "Restarted contrail-vrouter-agent container"
 
 else 
   docker run --privileged -d -P --name contrail_vrouter_agent --net="host" -t -i -e sysimage=/host -v /etc/contrail:/etc/contrail -v /var/log/contrail:/var/log/contrail $img:$ver /usr/bin/contrail-vrouter-agent 
+   echo "Started contrail-vrouter-agent in docker"
 fi
-echo "Contrail-vrouter agent in docker is up"
- 
-      
-   
