2b1df6937142252c1e0c7f06e01c2efe46eecda9
Sanju Abraham <asanju@gmail.com>
2015-10-19 09:26:13 -0700

    removing restart of docker

--- cluster/provision_minion.sh
+++ cluster/provision_minion.sh
@@ -528,7 +528,7 @@ function vr_agent_conf_image_pull()
           pkill -TERM -P $pullpid
        fi
        #check_docker
-       service docker restart
+       #service docker restart
        log_info_msg "pulling of opencontrail/vrouter-agent image was not successful in the initial attempt. Restarting docker to recover and retrying"
        (echo $vrimg | xargs -n1 sudo docker pull) & pullpid=$!
        i=0
