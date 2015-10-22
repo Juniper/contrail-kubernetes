4c77dd60fc8d75ecc16f06f4f52092b8aa161378
Sanju Abraham <asanju@gmail.com>
2015-10-21 23:02:56 -0700

    update vr agent config file

--- cluster/provision_minion.sh
+++ cluster/provision_minion.sh
@@ -463,7 +463,6 @@ function vr_agent_conf_image_pull()
   ur="Usable range"
   if [ -f $vrac ]; then
       sed -i 's/# tunnel_type=/tunnel_type=MPLSoUDP/g' $vrac
-      sed -i 's/# server=10.0.0.1 10.0.0.2/server='$OPENCONTRAIL_CONTROLLER_IP'/g' $vrac
       sed -i 's/# collectors=127.0.0.1:8086/collectors='$OPENCONTRAIL_CONTROLLER_IP':8086/g' $vrac
       sed -i 's/# type=kvm/type=docker/g' $vrac
       sed -i 's/# control_network_ip=/control_network_ip='$MINION_OVERLAY_NET_IP'/g' $vrac
