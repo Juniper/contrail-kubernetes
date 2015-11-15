c545a71be614a4ee2e2c2ca5eb08f9cfdfc581dd
Sanju Abraham <asanju@gmail.com>
2015-11-15 13:11:15 -0800

    reboot on grub update

--- cluster/provision_master.sh
+++ cluster/provision_master.sh
@@ -100,12 +100,13 @@ function configure-cgroup() {
   set +e
   echo "=== checking grub config for cgroup ==="
   cg=$(cat /etc/default/grub  | grep cgroup)
-  if [ -z "$cg" ]; then
+  if [[ -z "$cg" ]]; then
      source /etc/default/grub
      grubstr='GRUB_CMDLINE_LINUX_DEFAULT="'"$GRUB_CMDLINE_LINUX_DEFAULT cgroup_enable=memory swapaccount=1"'"'
      sed -i '/GRUB_CMDLINE_LINUX_DEFAULT/d' /etc/default/grub
      echo $grubstr >> /etc/default/grub
      sudo update-grub
+     reboot
   fi
   set -e
 }
